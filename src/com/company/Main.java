1a590e9d917dc1795833fd9143ccfc638750cbb9
Công Trần <trancong.uet@gmail.com>
2021-06-25 13:09:01 +0700

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,10 @@
 package com.company;
 
 public class Main {
-    enum Operater { add, subtract, multiple, divide, pow}
+    enum Operator { add, subtract, multiple, divide, pow}
 
-    static int calculate(Operater o, int... numbers){
+    static int calculate(Operator o, int... numbers){
+        // write calculator here
      switch (o){
          case add:
              int sum = 0;
@@ -18,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
-        System.out.println(calculate(Operater.add, 10,12,12,14));
+        System.out.println(calculate(Operator.add, 10,12,12,14));
     }
 };
 
