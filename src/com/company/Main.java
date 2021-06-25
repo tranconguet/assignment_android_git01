bebbaecc0f1403ca3c99f4164d3b7677c1e25172
Công Trần <trancong.uet@gmail.com>
2021-06-25 14:47:03 +0700

    add subtract operator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
     enum Operator { add, subtract, multiple, divide, pow}
 
     static int calculate(Operator o, int... numbers){
+        if(numbers.length <= 0)
+            return 0;
         // write calculator here
      switch (o){
          case add:
@@ -12,14 +14,21 @@ public class Main {
                  sum += numbers[i];
              }
              return sum;
+         case subtract:
+             int sum1 = numbers[0];
+             for (int i = 1; i < numbers.length; i++) {
+                 sum1 -= numbers[i];
+             }
+             return sum1;
+         default:
+             return 0;
      }
-     return 0;
     };
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
-        System.out.println(calculate(Operator.add, 10,12,12,14));
+        System.out.println(calculate(Operator.subtract, 10,12,12,14));
     }
 };
 
